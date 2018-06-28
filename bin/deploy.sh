2392b305f88034ff3e85ecdfb1edb6f886f576f7
Valery Melou <valery@valerymelou.com>
2018-06-28 09:10:51 +0100

    Update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,4 +5,4 @@ git branch --delete --force gh-pages
 git checkout --orphan gh-pages
 git add -f www
 git commit -m "Rebuild GitHub pages"
-git filter-branch -f --prune-empty --subdirectory-filter www && git push -f origin gh-pages && git checkout -
+git filter-branch -f --prune-empty --subdirectory-filter www && git push -f origin gh-pages && git checkout master
